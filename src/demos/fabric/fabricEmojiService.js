7fcf5c2dc326ad7fbfe8f90462b4a7e512263646
joohee-kim <joohee.kim15@navercorp.com>
2018-11-24 18:27:27 +0900

    fabric instance 사용하는 코드 분리

---
+++ src/demos/fabric/fabricEmojiService.js
@@ -0,0 +1,131 @@
+
+const emoji_preset = ["0x1F601", "0x1F602", "0x1F603", "0x1F604", "0x1F605", "0x1F606", "0x1F609", "0x1F60A", "0x1F60B", "0x1F60C", "0x1F60D", "0x1F60F", "0x1F612", "0x1F613", "0x1F614", "0x1F616", "0x1F618", "0x1F61A", "0x1F61C", "0x1F61D", "0x1F61E", "0x1F620", "0x1F621", "0x1F622", "0x1F623", "0x1F624", "0x1F625", "0x1F628", "0x1F629", "0x1F62A", "0x1F62B", "0x1F62D", "0x1F630", "0x1F631", "0x1F632", "0x1F633", "0x1F635", "0x1F637", "0x1F638", "0x1F639", "0x1F63A", "0x1F63B", "0x1F63C", "0x1F63D", "0x1F63E", "0x1F63F", "0x1F640", "0x1F645", "0x1F646", "0x1F647", "0x1F648", "0x1F649", "0x1F64A", "0x1F64B", "0x1F64C", "0x1F64D", "0x1F64E", "0x1F64F"];
+let emoji;
+let fabricInstance;
+
+function shuffle(a) {
+  var array = a.concat([]);
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+export function initFabric() {
+
+  fabricInstance = new fabric.Canvas('fabric', {
+    width: 640,
+    height: 480
+  });
+  // fix - lower-canvas 가 1280 으로 맞춰지는 이슈 (왜??)
+  fabricInstance.getContext().canvas.width = 640;
+  fabricInstance.getContext().canvas.height = 480;
+
+  loadSVG();
+  bindDeleteAction();
+//   bindAddAction();
+  emoji = shuffle(emoji_preset);
+  addRandomEmoji();
+  addRandomEmoji();
+  addRandomEmoji();
+}
+
+  
+  function loadSVG(e) {
+    const target = document.getElementById("delete");
+  
+    fabric.loadSVGFromString(target.outerHTML, function(objects, options) {
+      var svg = fabric.util.groupSVGElements(objects, options);
+      var config = {
+        left: fabricInstance.getWidth() / 2,
+        top: fabricInstance.getHeight() - 30,
+        fill: '#77f',
+        originX: 'center',
+        originY: 'center',
+        selectable: false,
+        hasBorders: false,
+        opacity: 0,
+        hasControls: false,
+        name: "deleteIcon",
+      };
+      svg.set(config);
+      var bg = new fabric.Rect({
+        ...config,
+        width: 50,
+        height: 50,
+        name: "deleteIcon",
+      })
+  
+      svg.scaleToWidth(50);
+      svg.scaleToHeight(50);
+      // svg.hasBorders = svg.hasControls = false;
+      fabricInstance.add(bg, svg).renderAll();
+    });
+  }
+  
+  
+  export function addRandomEmoji() {
+    if (emoji.length < 1) {
+      emoji = shuffle(emoji_preset);
+    }
+    const size = randomInt(25, 50);
+    // const size = 300;
+    const random = new fabric.Textbox(String.fromCodePoint(emoji.pop()), {
+      top: randomInt(10, 470),
+      left: randomInt(10, 630),
+      angle: randomInt(-180, 180),
+      fontSize: size,
+      hasBorders: false,
+      hasControls: false,
+    });
+    fabricInstance
+      .add(random).renderAll();
+  }
+  
+  function bindDeleteAction() {
+    // http://fabricjs.com/intersection
+    fabricInstance.on({
+      'object:moving': function(e) {
+        e.target.setCoords();
+        fabricInstance.forEachObject(function(obj) {
+          if (obj === e.target || obj.name !== "deleteIcon") return;
+          obj.set('opacity' ,e.target.intersectsWithObject(obj) ? 0.8 : 0.4);
+        });
+      },
+      'object:moved' : function(e) {
+        e.target.setCoords();
+        const icons = fabricInstance.getObjects().filter(function(obj) {
+          return obj.name === "deleteIcon";
+        });
+        if(e.target.intersectsWithObject(icons[0])) {
+          fabricInstance.remove(e.target);
+        }
+        icons.forEach(function(icon) {
+          icon.set("opacity", 0);
+        })
+      }
+    })
+  }
+  
+  export function getFabricCanvas() {
+      return fabricInstance.getContext().canvas;
+  }
--- src/demos/fabric/index.js
+++ src/demos/fabric/index.js
@@ -1,125 +1,23 @@
 /*
 ref) https://sudo.isl.co/webrtc-real-time-image-filtering/
 */
-import dat from "dat.gui";
+import {
+  installMediaDevice,
+  getMediaElement,
+} from "../util";
+import {initFabric, getFabricCanvas, addRandomEmoji} from "./fabricEmojiService";
 
-const save = document.getElementById('save');
-const video = document.getElementById('video');
-const buffer = document.createElement('canvas');
-const output = document.getElementById('canvas');
-const addButton = document.querySelector("#add");
+let {video, buffer, output} = getMediaElement(); 
 
-let emoji_preset = [0x1F600, 0x1F604, 0x1F34A, 0x1F344, 0x1F37F, 0x1F363, 0x1F370, 0x1F355,
-  0x1F354, 0x1F35F, 0x1F6C0, 0x1F48E, 0x1F5FA, 0x23F0, 0x1F579, 0x1F4DA,
-  0x1F431, 0x1F42A, 0x1F439, 0x1F424];
-let emoji;
-let fabricInstance;
-
-function shuffle(a) {
-  var array = a.concat([]);
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-
-function initFabric() {
-
-  fabricInstance = new fabric.Canvas('fabric', {
-    width: 640,
-    height: 480
-  });
-  // fix - lower-canvas 가 1280 으로 맞춰지는 이슈 (왜??)
-  fabricInstance.getContext().canvas.width = 640;
-  fabricInstance.getContext().canvas.height = 480;
-
-  // const red = new fabric.Rect({
-  //   top: 100,
-  //   left: 0,
-  //   width: 80,
-  //   height: 50,
-  //   fill: 'red'
-  // });
-  // const blue = new fabric.Rect({
-  //   top: 0,
-  //   left: 100,
-  //   width: 50,
-  //   height: 70,
-  //   fill: 'blue'
-  // });
-  // const green = new fabric.Rect({
-  //   top: 100,
-  //   left: 100,
-  //   width: 60,
-  //   height: 60,
-  //   fill: 'green'
-  // });
-
-  // fabricInstance
-  //   .add(red, blue, green);
-  
-  // fabricInstance.forEachObject(function (o) {
-  //   o.hasBorders = o.hasControls = false;
-  // });
-  loadSVG();
-  bindDeleteAction();
-  bindAddAction();
-  addRandomEmoji();
-  addRandomEmoji();
-  addRandomEmoji();
-}
-
-function init() {
-  window.navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        facing: 'user'
-      }
-    })
-    .then(stream => {
-      window.devicePixelRatio = 1;
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch(err => {
-      if (err.code === 0) {
-        document.getElementById('loading').style.display = "none";
-        document.getElementById('permission').style.display = "block";
-      }
-      console.log('There was an error 😱', err)
-    });
-}
-
-function setup(stream) {
-  video.srcObject = stream;
-  video.play();
-
-  // window.devicePixelRatio = 1;
-}
-
+const saveButton = document.getElementById('save');
+const addButton = document.getElementById('add');
 function render() {
-  buffer.width = video.videoWidth;
-  buffer.height = video.videoHeight;
-  output.width = video.videoWidth;
-  output.height = video.videoHeight;
-
   const bufferContext = buffer.getContext('2d');
   const outputContext = output.getContext('2d');
 
   // 거울모드
+  buffer.width = video.videoWidth;
+  buffer.height = video.videoHeight;
   bufferContext.translate(video.videoWidth, 0);
   bufferContext.scale(-1, 1);
 
@@ -130,16 +28,16 @@ function render() {
   window.requestAnimationFrame(render);
 }
 
-function saveImage() {
+export function saveImage() {
   const a = document.createElement('a');
   const film = document.createElement('canvas');
   const filmContext = film.getContext('2d');
-  const fabricContext = fabricInstance.getContext();
+  const fabric = getFabricCanvas();
 
   film.width = video.videoWidth;
   film.height = video.videoHeight;
   filmContext.drawImage(output, 0, 0);
-  filmContext.drawImage(fabricContext.canvas, 0, 0);
+  filmContext.drawImage(fabric, 0, 0);
 
   // https://stackoverflow.com/questions/10673122/how-to-save-canvas-as-an-image-with-canvas-todataurl
   // here is the most important part because if you dont replace you will get a DOM 18 exception.
@@ -148,104 +46,16 @@ function saveImage() {
   a.setAttribute('download', 'canvasTest.png');
   a.setAttribute('href', dataURL);
   a.click();
-
-
 }
 
-document.addEventListener("DOMContentLoaded", init);
-save.addEventListener("click", saveImage);
-video.addEventListener('canplay', function () {
-  document.getElementById('loading').style.display = "none";
-  document.getElementById('content').style.display = "block";
-  // document.getElementById("file").onchange = openSVG;
+function canplay () {
   render();
   initFabric();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  saveButton.addEventListener("click", saveImage);
+  addButton.addEventListener('click', addRandomEmoji);
+  video.addEventListener('canplay', canplay);
+  installMediaDevice();
 });
-
-function loadSVG(e) {
-  const target = document.getElementById("delete");
-
-  fabric.loadSVGFromString(target.outerHTML, function(objects, options) {
-    var svg = fabric.util.groupSVGElements(objects, options);
-    var config = {
-      left: fabricInstance.getWidth() / 2,
-      top: fabricInstance.getHeight() - 30,
-      fill: '#77f',
-      originX: 'center',
-      originY: 'center',
-      selectable: false,
-      hasBorders: false,
-      opacity: 0,
-      hasControls: false,
-      name: "deleteIcon",
-    };
-    svg.set(config);
-    var bg = new fabric.Rect({
-      ...config,
-      width: 50,
-      height: 50,
-      name: "deleteIcon",
-    })
-
-    svg.scaleToWidth(50);
-    svg.scaleToHeight(50);
-    // svg.hasBorders = svg.hasControls = false;
-    fabricInstance.add(bg, svg).renderAll();
-  });
-}
-
-function bindAddAction() {
-  emoji = shuffle(emoji_preset);
-  addButton.addEventListener('click', function() {
-    addRandomEmoji();
-  });
-}
-
-function randomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function addRandomEmoji() {
-  if (emoji.length < 1) {
-    emoji = shuffle(emoji_preset);
-  }
-  const size = randomInt(25, 50);
-  // const size = 300;
-  const random = new fabric.Textbox(String.fromCodePoint(emoji.pop()), {
-    top: randomInt(10, 470),
-    left: randomInt(10, 630),
-    angle: randomInt(-180, 180),
-    fontSize: size,
-    hasBorders: false,
-    hasControls: false,
-  });
-  fabricInstance
-    .add(random).renderAll();
-}
-
-function bindDeleteAction() {
-  // http://fabricjs.com/intersection
-  fabricInstance.on({
-    'object:moving': function(e) {
-      e.target.setCoords();
-      fabricInstance.forEachObject(function(obj) {
-        if (obj === e.target || obj.name !== "deleteIcon") return;
-        obj.set('opacity' ,e.target.intersectsWithObject(obj) ? 0.8 : 0.4);
-      });
-    },
-    'object:moved' : function(e) {
-      e.target.setCoords();
-      const icons = fabricInstance.getObjects().filter(function(obj) {
-        return obj.name === "deleteIcon";
-      });
-      if(e.target.intersectsWithObject(icons[0])) {
-        fabricInstance.remove(e.target);
-      }
-      icons.forEach(function(icon) {
-        icon.set("opacity", 0);
-      })
-    }
-  })
-}
